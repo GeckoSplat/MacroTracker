73cbfefa94f85b7148b7d91149e1f54988cc47dd
GeckoSplat <111183176+GeckoSplat@users.noreply.github.com>
2023-05-03 00:45:58 +0100

    code quality

--- createDB.py
+++ createDB.py
@@ -1,12 +1,17 @@
-import sqlite3 as sql 
-        
-def database(): # only needed to first create DB on computer
-    con = sql.connect('macrosdb')
+import sqlite3 as sql
+
+
+def database():  # only needed to first create DB on computer
+    con = sql.connect("macrosdb")
     cur = con.cursor()
-    cur.execute(""" CREATE TABLE Kcal(                       
+    cur.execute(
+        """ CREATE TABLE Kcal(                       
     Kcals , Fats , Carbs , Proteins )    
-    """)        
+    """
+    )
     con.commit()
     con.close()
-    print(' DB DONE' )
-database()   
+    print(" DB DONE")
+
+
+database()
--- main.py
+++ main.py
@@ -1,111 +1,114 @@
+import kivy
+from kivy.app import App
 
-import kivy  
-from kivy.app import App 
-kivy.require('2.1.0')
+kivy.require("2.1.0")
 from kivy.uix.widget import Widget
-from kivy.uix.textinput import TextInput 
-from kivy.uix.relativelayout import RelativeLayout
-from kivy.uix.button import ButtonBehavior 
-from kivy.uix.label import Label
-from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import ButtonBehavior
 import sqlite3 as sql
 
 
-
- #Create the widget class
+# Create the widget class
 class textinp(Widget):
     TextInput.multiline = False
-    pass 
+    pass
+
 
 # Create button Class
 class buttons(ButtonBehavior):
-    pass    
-  
+    pass
+
+
 class MainApp(App):
-    
-    
     def build(self):
-        self.icon = 'PNGs/FoodIcon-32.png'
+        self.icon = "PNGs/FoodIcon-32.png"
         return textinp()
-        
-    def add_data(self):# Adds data input from textbox's to database 
+
+    def add_data(self):  # Adds data input from textbox's to database
         text1 = self.root.ids.input1.text
-        text2 = self.root.ids.input2.text 
+        text2 = self.root.ids.input2.text
         text3 = self.root.ids.input3.text
         text4 = self.root.ids.input4.text
-        con = sql.connect('macrosdb')
+        con = sql.connect("macrosdb")
         cur = con.cursor()
-        cur.execute("""INSERT INTO Kcal (Kcals,Fats,Carbs,Proteins) VALUES (?,?,?,?)""",
-        (text4,text3,text2,text1))
+        cur.execute(
+            """INSERT INTO Kcal (Kcals,Fats,Carbs,Proteins) VALUES (?,?,?,?)""",
+            (text4, text3, text2, text1),
+        )
         con.commit()
-        cur.execute('select * from Kcal')
+        cur.execute("select * from Kcal")
         db_check = cur.fetchall()
         print(db_check)
         con.close()
-  
-    def remove_last_data(self):# rowid important here
-        con = sql.connect('macrosdb')
+
+    def remove_last_data(self):  # rowid important here
+        con = sql.connect("macrosdb")
         cur = con.cursor()
-        cur.execute("""DELETE FROM Kcal WHERE rowid = (SELECT MAX(rowid) FROM Kcal);""") 
+        cur.execute("""DELETE FROM Kcal WHERE rowid = (SELECT MAX(rowid) FROM Kcal);""")
         con.commit()
         con.close()
-        print(' DB ROW DELETED')
+        print(" DB ROW DELETED")
 
-    def clear_data(self):# Clears data from database to start new day of tracking
-        con = sql.connect('macrosdb')
-        cur = con.cursor()   
+    def clear_data(self):  # Clears data from database to start new day of tracking
+        con = sql.connect("macrosdb")
+        cur = con.cursor()
         cur.execute("""DELETE FROM Kcal;""")
         con.commit()
         con.close()
-        print(' ALL DATA DELETED')
+        print(" ALL DATA DELETED")
 
-    def clear_text_boxes(self):# Clears text inputs after button pressed
-        self.root.ids.input1.text = ''
-        self.root.ids.input2.text = ''
-        self.root.ids.input3.text = ''
-        self.root.ids.input4.text = ''         
+    def clear_text_boxes(self):  # Clears text inputs after button pressed
+        self.root.ids.input1.text = ""
+        self.root.ids.input2.text = ""
+        self.root.ids.input3.text = ""
+        self.root.ids.input4.text = ""
 
-    def totalsbuttonKcal(self): # TotalButton for loops, only 1 button for 4 total displays
-        con = sql.connect('macrosdb')
-        cur = con.cursor()   
-        cur.execute('SELECT SUM (Kcals) FROM Kcal;')
+    def totalsbuttonKcal(
+        self,
+    ):  # TotalButton for loops, only 1 button for 4 total displays
+        con = sql.connect("macrosdb")
+        cur = con.cursor()
+        cur.execute("SELECT SUM (Kcals) FROM Kcal;")
         Total = cur.fetchall()
-        global output
-        output = 'NONE'
-        for number in Total: 
-            try:                   
+        output = "NONE"
+        for number in Total:
+            try:
                 output = f"{round(number[0],1)} {'Kcals'}"
-                self.root.ids.outputKcals.text = f'{output}'
-            except: TypeError
-            self.root.ids.outputKcals.text = f'{output}'
+                self.root.ids.outputKcals.text = f"{output}"
+            except:
+                TypeError
+            self.root.ids.outputKcals.text = f"{output}"
         print(Total)
-        cur.execute('SELECT SUM (Carbs) FROM Kcal;')
+        cur.execute("SELECT SUM (Carbs) FROM Kcal;")
         Total = cur.fetchall()
         for number in Total:
-            try:                    
+            try:
                 output = f"{round(number[0],1)} {'Carbs'}"
-                self.root.ids.TOTcbButton.text = f'{output}'
-            except: TypeError
-            self.root.ids.TOTcbButton.text = f'{output}'
+                self.root.ids.TOTcbButton.text = f"{output}"
+            except:
+                TypeError
+            self.root.ids.TOTcbButton.text = f"{output}"
         print(Total)
-        cur.execute('SELECT SUM (Fats) FROM Kcal;')
+        cur.execute("SELECT SUM (Fats) FROM Kcal;")
         Total = cur.fetchall()
         for number in Total:
-            try:                  
+            try:
                 output = f"{round(number[0],1)} {'Fats'}"
-                self.root.ids.TOTfButton.text = f'{output}'
-            except: TypeError
-            self.root.ids.TOTfButton.text = f'{output}'    
+                self.root.ids.TOTfButton.text = f"{output}"
+            except:
+                TypeError
+            self.root.ids.TOTfButton.text = f"{output}"
         print(Total)
-        cur.execute('SELECT SUM (Proteins) FROM Kcal;')
+        cur.execute("SELECT SUM (Proteins) FROM Kcal;")
         Total = cur.fetchall()
-        for number in Total:                   
+        for number in Total:
             try:
                 output = f"{round(number[0],1)} {'Protein'}"
-                self.root.ids.TOTpButton.text = f'{output}'
-            except:TypeError
-            self.root.ids.TOTpButton.text = f'{output}'
-        print(Total)      
+                self.root.ids.TOTpButton.text = f"{output}"
+            except:
+                TypeError
+            self.root.ids.TOTpButton.text = f"{output}"
+        print(Total)
 
 
 if __name__ == "__main__":
